1ca26facfdb1ec7977be9d5f970382f7288f08ac
QiuJianrong <g21307130151mail@gmail.com>
2026-01-29 19:35:19 +0800

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,8 +9,8 @@ export function Footer(): ReactElement {
             <div className="footer-left-text">
               <p>
                 {' '}
-                © CASTLE Group |{' '}
-                <a href="http://www.cse.ust.hk">CSE in HKUST</a> | Last Updated:{' '}
+                © SWIFT Group |{' '}
+                <a href="http://www.cse.ust.hk">University Link</a> | Last Updated:{' '}
                 <span id="build-time">{process.env.REACT_APP_BUILD_TIME}</span>{' '}
                 | Built Version Hash:{' '}
                 <span id="build-hash">{process.env.REACT_APP_GIT_SHA}</span>{' '}
