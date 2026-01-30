ab3317938611a02a3d5bdf273bf2e7e6e54340bb
邱剑荣 <g21307130151mail@gmail.com>
2026-01-30 08:46:16 +0000

    去掉href

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,7 @@ export function Home(): ReactElement {
                 <div className="card-body">
                     <h5 className="card-title" style={{alignItems:'center'}}>Card title</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="card-link">Card link</a>
+                    {/* <a href="#" className="card-link">Card link</a> */}
                 </div>
                 </div>
             </Carousel.Item>
