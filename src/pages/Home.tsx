161420d18f6acc264f7d330f1788fce76dfe878c
邱剑荣 <g21307130151mail@gmail.com>
2026-01-30 07:27:49 +0000

    1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -51,7 +51,7 @@ export function Home(): ReactElement {
             {/* <img src="images/about.jpg" alt=" " /> */}
             {/* React Bootstrap 轮播图 */}
             <Carousel interval={3000} controls={true} indicators={true}>
-              <Carousel.Item style={{ height: '300px' }}>
+              <Carousel.Item style={{ width: '400px', height: '300px' }}>
                 <img
                   className="d-block w-100 h-100"
                   src="images/about.jpg"
@@ -59,7 +59,7 @@ export function Home(): ReactElement {
                   style={{ objectFit: 'contain', backgroundColor: '#fbfbfb' }}
                 />
               </Carousel.Item>
-              <Carousel.Item style={{ height: '300px' }}>
+              <Carousel.Item style={{ width: '400px', height: '300px' }}>
                 <img
                   className="d-block w-100 h-100"
                   src="images/1.jpg"
@@ -67,7 +67,7 @@ export function Home(): ReactElement {
                   style={{ objectFit: 'contain', backgroundColor: '#fbfbfb' }}
                 />
               </Carousel.Item>
-              <Carousel.Item style={{ height: '300px' }}>
+              <Carousel.Item style={{ width: '400px', height: '300px' }}>
                 <img
                   className="d-block w-100 h-100"
                   src="images/logo.jpg"
