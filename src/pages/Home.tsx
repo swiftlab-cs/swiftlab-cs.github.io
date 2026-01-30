d8ab1f567b455d16199847a02347f8d896524096
邱剑荣 <g21307130151mail@gmail.com>
2026-01-30 08:43:06 +0000

    className

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -28,11 +28,11 @@ export function Home(): ReactElement {
         {/* React Bootstrap 轮播图 */}
         <Carousel interval={3000} controls={true} indicators={true}>
             <Carousel.Item style={{ height: '300px' }}>
-                <div class="card" style={{width: "18rem"}}>
-                <div class="card-body">
-                    <h5 class="card-title" style={{alignItems:'center'}}>Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" class="card-link">Card link</a>
+                <div className="card" style={{width: "18rem"}}>
+                <div className="card-body">
+                    <h5 className="card-title" style={{alignItems:'center'}}>Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="card-link">Card link</a>
                 </div>
                 </div>
             </Carousel.Item>
