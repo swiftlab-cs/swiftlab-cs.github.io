4c6c4df864fe7b55b7e8f54ba206ab8fce07c826
邱剑荣 <g21307130151mail@gmail.com>
2026-01-31 06:41:56 +0000

    PJ名字

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,15 +7,9 @@ export function Projects(): ReactElement {
         <h4>Funded Research Projects</h4>
         <ul className="project_ul">
           <li>
-            National Natural Science Foundation of China (NSFC) for Ph.D. Graduate Students「国家自然科学基金青年学生基础研究项目（博士研究生）」, 2024
+            Optimizing the I/O Efficiency of Key-Value Stores on Disaggregated Memory. National Natural Science Foundation of China (NSFC) for Ph.D. Graduate Students「国家自然科学基金青年学生基础研究项目（博士研究生）」, 2024
           </li>
 
-          <li>
-            More recent funded projects are avaiable at{' '}
-            <a href="https://facultyprofiles.hkust.edu.hk/profiles.php?profile=shing-chi-cheung-sccheung#proj">
-              here
-            </a>
-          </li>
         </ul>
       </div>
     </>
