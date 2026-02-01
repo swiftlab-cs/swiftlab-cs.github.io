fbe349f011acee3f64cf01125f3f1dc38622780e
邱剑荣 <g21307130151mail@gmail.com>
2026-02-01 12:54:16 +0000

    1

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,11 +22,11 @@ export function Home(): ReactElement {
         <h4>News</h4>
         <ul>
             <li>
-                <strong>Jan. 2026: </strong> <a href="https://openreview.net/pdf?id=GymjF88oGQ">One paper</a> is accepted by ICLR 2026.
+                <b>Jan. 2026: </b> <a href="https://openreview.net/pdf?id=GymjF88oGQ">One paper</a> is accepted by ICLR 2026.
             </li>
 
             <li>
-                <strong>Jan. 2026: </strong> SWIFT Lab Founded!
+                <b>Jan. 2026: </b> SWIFT Lab Founded!
             </li>
 
         </ul>
