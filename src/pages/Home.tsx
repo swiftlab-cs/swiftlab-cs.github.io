024e809699a04274bf7c66d93ea1ed5658f19132
Jiacheng Shen <js1242@duke.edu>
2026-02-06 15:21:27 +0800

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ export function Home(): ReactElement {
         <h4>News</h4>
         <ul>
             <li>
-                <b>Feb. 2026: </b> We are <strong>recruiting PhD students</strong> working on efficient machine learning systems in 2026 Fall. Feel free to email us if you are interested!
+                <b>Feb. 2026: </b> We are seeking motivated PhD candidates to join our research on <strong>efficient machine learning systems</strong>, starting in <strong>Fall 2026</strong>. If you are interested, please contact us for more information.
             </li>
             <li>
                 <b>Jan. 2026: </b> <a href="https://openreview.net/pdf?id=GymjF88oGQ">One paper</a> is accepted by ICLR 2026.
